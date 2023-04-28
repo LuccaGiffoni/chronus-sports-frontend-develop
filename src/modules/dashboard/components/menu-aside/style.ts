d501dd7120e3869e5a6bc7a08cf6325cf1eacf8a
Álex Maciel <awasley.gina@gmail.com>
2023-04-28 03:34:20 -0300

    Criando menu aside da page dashboard

---
+++ src/modules/dashboard/components/menu-aside/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    display: grid;
+    padding: 32px;
+    width: 20%;
+`
+
+export const Items = styled.div`
+    display: flex;
+
+    a {
+        color: #EEEEEE;
+        font-weight: 400;
+        font-size: 16px;
+        margin: 30px 8px;
+    }
+`
